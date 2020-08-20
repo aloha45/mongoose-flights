a8dcd3e8ba3a618bce1bace2ce254e34a1c09abf
Cory Spicer <corys4139@gmail.com>
2020-08-20 16:00:22 -0400

    show functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -4,14 +4,20 @@ const Flight = require("../models/flight");
 module.exports = {
     new: newFlight,
     create,
-    index
+    index,
+    show
 };
 
 function newFlight(req, res) {
-    res.render('flights/new');
+    res.render('flights/new', { title: 'Add Flight'});
 };
 
 function create(req, res) {
+    if (!req.body.departs) {
+        let redate = new Date();
+        redate.setFullYear(redate.getFullYear()+1);
+        req.body.departs = redate
+    }
     Flight.create(req.body)
     .then(flight => {
         console.log(flight)
@@ -23,17 +29,15 @@ function create(req, res) {
     })
 }
 
-function createFlight (req, res) {
-    if (!req.body.departs) {
-        redate.setFullYear(redate.getFullYear()+1);
-        req.body.departs = redate
-        return redate
-    }
-}
-
 function index(req, res){
     Flight.find({})
     .then(flights => {
         res.render('flights/index', {title: 'All Flights', flights})
     })
+}
+
+function show(req, res) {
+    Flight.findById(req.params.id, function (err, flight) {
+        res.render('flights/show', { title: 'Flight Detail', flight });
+    });
 }
--- models/flight.js
+++ models/flight.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const flightSchema = {
+const flightSchema = new Schema({
     airline: {type: String, enum: ['Southwest', 'United', 'American', 'Dodo']},
     airport: {type: String, enum: ['AUS', 'CMH', 'DEN', 'LAX', 'JFK', 'LGA']},
     flightNo: {type: Number, min: 10, max: 9999, required: true},
-    departs: {type: Date, default: new Date(Date.now() + 365*24*60*60*1000), required: true}, 
-    }
+    departs: Date
+})
 
 module.exports = mongoose.model('Flight', flightSchema)
