4c81392474eb904c1db6c1fdf70c59e4b15edcf8
Cory Spicer <corys4139@gmail.com>
2020-08-20 16:47:45 -0400

    add destination schema

--- models/flight.js
+++ models/flight.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const destinationSchema = new Schema({
+    airport: {type: String, enum: ['AUS', 'CMH', 'DEN', 'LAX', 'JFK', 'LGA']},
+    arrival: Date
+}, {
+    timestamps:true
+});
+
 const flightSchema = new Schema({
     airline: {type: String, enum: ['Southwest', 'United', 'American', 'Dodo']},
     airport: {type: String, enum: ['AUS', 'CMH', 'DEN', 'LAX', 'JFK', 'LGA']},
     flightNo: {type: Number, min: 10, max: 9999, required: true},
-    departs: Date
-})
+    departs: Date,
+    destinations: [destinationSchema]
+}, {
+    timestamps: true
+});
 
 module.exports = mongoose.model('Flight', flightSchema)
