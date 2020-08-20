d3e917686d50f5406b22efc96cea43927c7df565
Cory Spicer <corys4139@gmail.com>
2020-08-20 11:15:01 -0400

    deploy to heroku

--- models/flight.js
+++ models/flight.js
@@ -4,13 +4,7 @@ const Schema = mongoose.Schema
 const flightSchema = {
     airline: {type: String, enum: ['Southwest', 'United', 'American', 'Dodo']},
     airport: {type: String, enum: ['AUS', 'CMH', 'DEN', 'LAX', 'JFK', 'LGA']},
-    flightNo: {type: Number, 
-        default: parseInt(Math.floor(Math.random()*(9999 - 10) + 10))
-        },
-    departs: {type: Date, 
-        default: function() {
-            return (Date.now() + 365*24*60*60*1000)
-}}
-}
-
+    flightNo: {type: Number, min: 10, max: 9999, required: true},
+    departs: {type: Date, default: new Date(Date.now() + 365*24*60*60*1000), required: true}, 
+    }
 module.exports = mongoose.model('Flight', flightSchema)
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('./config/database')
+require('dotenv').config();
+require('./config/database');
+const methodOverride = require('method-override');
 
 var indexRouter = require('./routes/index');
 var flightsRouter = require('./routes/flights');
@@ -19,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/flights', flightsRouter);
